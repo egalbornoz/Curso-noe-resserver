ad541b9678b9f775be94b0d63d2a768375e4abc2
Edwin <egalbornoz@gmail.com>
2021-03-17 23:51:36 -0400

    Implementación Google Sigin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const bcrypjs = require('bcryptjs');
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async (req, res = response) => {
 
@@ -31,7 +32,7 @@ const login = async (req, res = response) => {
             })
         }
         // Generar JWT,
-        const token = await generarJWT(usuario.id)
+        const token = await generarJWT(usuario.id);
 
         res.json({
             usuario,
@@ -39,13 +40,63 @@ const login = async (req, res = response) => {
         });
 
     } catch (error) {
-        console.log(error);
+        // console.log(error);
         return res.status(500).json({
             msg: 'Algo salio mal, comunicar al administrador'
         })
     }
 }
 
+const googleSigin = async (req, res = response) => {
+    const { id_token } = req.body;
+    try {
+        const { correo, nombre, img } = await googleVerify(id_token);
+
+        // Verificar que el correo no este registrado en la db
+
+        let usuario = await Usuario.findOne({ correo });
+
+        // //Si el Usuario válidado via google no existe, lo creo
+        if (!usuario) {
+            const data = {
+                nombre,
+                correo,
+                contraseña: ':P',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // // Si el usuario en Db esta en estado false(eliminado)
+
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, Usuario bloqueado',
+            });
+        }
+        // Generar JWT,
+        const token = await generarJWT(usuario.id);
+
+
+
+        res.json({
+            usuario,
+            token
+        });
+
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'Token de google no es válido'
+        })
+    }
+
+
+}
+
 module.exports = {
-    login
+    login,
+    googleSigin
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,24 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { route } = require('./usuarios');
-const { login } = require('../controllers/auth');
+const { login,googleSigin } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const router = Router();
 
-router.post('/login',[
-    check('correo','El correo es obligatorio').isEmail(),
-    check('contraseña','La contraseña es obligatoria').not().isEmpty(),
+router.post('/login', [
+    check('correo', 'El correo es obligatorio').isEmail(),
+    check('contraseña', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 
-], login );
+], login);
+
+router.post('/google', [
+    check('id_token', 'El id token es necesario').not().isEmpty(),
+    validarCampos
+
+], googleSigin);
 
 
 
